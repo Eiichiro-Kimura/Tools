23313616db8aaea443fd0dc667c89a080a34e368
Eiichiro Kimura <kmra1ro@gmail.com>
2021-01-16 21:32:04 +0900

    [fix] ルートビューをファイル分離

--- dazn_schedule/lib/main.dart
+++ dazn_schedule/lib/main.dart
@@ -1,22 +1,6 @@
-import 'package:dazn_schedule/page/page_manager.dart';
+import 'package:dazn_schedule/view/app_view.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Dazn Schedule',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      initialRoute: '/',
-      routes: PageManager().routes,
-    );
-  }
+  runApp(AppView());
 }
---
+++ dazn_schedule/lib/view/app_view.dart
@@ -0,0 +1,18 @@
+import 'package:dazn_schedule/page/page_manager.dart';
+import 'package:flutter/material.dart';
+
+class AppView extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Dazn Schedule',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      initialRoute: '/',
+      routes: PageManager().routes,
+    );
+  }
+}
