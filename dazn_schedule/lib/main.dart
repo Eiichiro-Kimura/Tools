2ad310ee09bc185a2549298b7d48269ec3f532f8
Eiichiro Kimura <kmra1ro@gmail.com>
2021-01-27 21:31:57 +0900

    [fix] HiveDatabaseからアダプタ追加処理を分離

--- dazn_schedule/lib/main.dart
+++ dazn_schedule/lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:dazn_schedule/model/favorite_team.dart';
 import 'package:dazn_schedule/model/io/hive_database.dart';
 import 'package:dazn_schedule/view/root/app_root.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
-  await HiveDatabase().init();
+  final hiveDatabase = HiveDatabase()
+    ..addAdapter(FavoriteTeamAdapter());
+  await hiveDatabase.init();
 
   runApp(const AppRoot());
 }
--- dazn_schedule/lib/model/io/hive_database.dart
+++ dazn_schedule/lib/model/io/hive_database.dart
@@ -1,4 +1,3 @@
-import 'package:dazn_schedule/model/favorite_team.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
@@ -9,11 +8,9 @@ class HiveDatabase {
 
   static final HiveDatabase _instance = HiveDatabase._internal();
 
-  Future<void> init() async {
-    await Hive.initFlutter();
+  Future<void> init() => Hive.initFlutter();
 
-    Hive.registerAdapter(FavoriteTeamAdapter());
-  }
+  Future<Box<T>> open<T>() => Hive.openBox<T>(T.toString());
 
-  Future<Box<T>> open<T>() async => Hive.openBox<T>(T.toString());
+  void addAdapter<T>(TypeAdapter<T> adapter) => Hive.registerAdapter(adapter);
 }
